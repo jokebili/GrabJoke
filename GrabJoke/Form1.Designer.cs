89a396808b2350843ef9a8ad19ac7c5ab22baaf7
Yongchao Liu <1057388362@qq.com>
2020-02-11 12:40:21 +0800

    Comment 新增浏览笑话内容功能

--- GrabJoke/Form1.Designer.cs
+++ GrabJoke/Form1.Designer.cs
@@ -31,13 +31,21 @@
             this.components = new System.ComponentModel.Container();
             this.btn_getdata = new System.Windows.Forms.Button();
             this.rt_txt = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_zan = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_yues = new System.Windows.Forms.Label();
+            this.lb_buzan = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lb_date = new System.Windows.Forms.Label();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // btn_getdata
@@ -52,21 +60,15 @@
             // 
             // rt_txt
             // 
+            this.rt_txt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.rt_txt.ForeColor = System.Drawing.Color.Blue;
             this.rt_txt.Location = new System.Drawing.Point(2, 91);
             this.rt_txt.Name = "rt_txt";
             this.rt_txt.ReadOnly = true;
-            this.rt_txt.Size = new System.Drawing.Size(440, 471);
+            this.rt_txt.Size = new System.Drawing.Size(440, 593);
             this.rt_txt.TabIndex = 3;
             this.rt_txt.Text = "";
-            // 
-            // label1
-            // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.Location = new System.Drawing.Point(444, -1);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(6, 563);
-            this.label1.TabIndex = 4;
             // 
             // linkLabel1
             // 
@@ -107,20 +109,119 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(448, 20);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.ShowLines = false;
+            this.treeView1.Size = new System.Drawing.Size(471, 281);
+            this.treeView1.TabIndex = 8;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "内容标题：";
+            // 
+            // lb_zan
+            // 
+            this.lb_zan.AutoSize = true;
+            this.lb_zan.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_zan.ForeColor = System.Drawing.Color.Red;
+            this.lb_zan.Location = new System.Drawing.Point(500, 318);
+            this.lb_zan.Name = "lb_zan";
+            this.lb_zan.Size = new System.Drawing.Size(16, 16);
+            this.lb_zan.TabIndex = 15;
+            this.lb_zan.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(448, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "点赞数：";
+            // 
+            // lb_yues
+            // 
+            this.lb_yues.AutoSize = true;
+            this.lb_yues.Location = new System.Drawing.Point(563, 320);
+            this.lb_yues.Name = "lb_yues";
+            this.lb_yues.Size = new System.Drawing.Size(53, 12);
+            this.lb_yues.TabIndex = 14;
+            this.lb_yues.Text = "不赞数：";
+            // 
+            // lb_buzan
+            // 
+            this.lb_buzan.AutoSize = true;
+            this.lb_buzan.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_buzan.ForeColor = System.Drawing.Color.Red;
+            this.lb_buzan.Location = new System.Drawing.Point(611, 317);
+            this.lb_buzan.Name = "lb_buzan";
+            this.lb_buzan.Size = new System.Drawing.Size(16, 16);
+            this.lb_buzan.TabIndex = 15;
+            this.lb_buzan.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(678, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "获取时间：";
+            // 
+            // lb_date
+            // 
+            this.lb_date.AutoSize = true;
+            this.lb_date.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_date.ForeColor = System.Drawing.Color.Red;
+            this.lb_date.Location = new System.Drawing.Point(741, 317);
+            this.lb_date.Name = "lb_date";
+            this.lb_date.Size = new System.Drawing.Size(16, 16);
+            this.lb_date.TabIndex = 15;
+            this.lb_date.Text = "0";
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser2.Location = new System.Drawing.Point(448, 346);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.ScrollBarsEnabled = false;
+            this.webBrowser2.Size = new System.Drawing.Size(471, 338);
+            this.webBrowser2.TabIndex = 16;
+            // 
             // txt_url
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(920, 560);
+            this.ClientSize = new System.Drawing.Size(920, 682);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.lb_date);
+            this.Controls.Add(this.lb_buzan);
+            this.Controls.Add(this.lb_zan);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lb_yues);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.rt_txt);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_getdata);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.MaximizeBox = false;
             this.Name = "txt_url";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "笑话段子抓取";
@@ -134,13 +235,21 @@
         #endregion
         private System.Windows.Forms.Button btn_getdata;
         private System.Windows.Forms.RichTextBox rt_txt;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_zan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_yues;
+        private System.Windows.Forms.Label lb_buzan;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lb_date;
+        private System.Windows.Forms.WebBrowser webBrowser2;
     }
 }
 
--- GrabJoke/Form1.cs
+++ GrabJoke/Form1.cs
@@ -2,6 +2,7 @@
 using Common.Util;
 using HtmlAgilityPack;
 using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,26 +20,77 @@ namespace GrabJoke
 {
     public partial class txt_url : Form
     {
+        Thread th;
+        long lines = 0;
+        ConcurrentQueue<JokeData> currendQueue = new ConcurrentQueue<JokeData>();
+
+        ArrayList list = new ArrayList();
+        delegate void ShowMessage(string message, string title, ArrayList list); 
+        delegate void showMes(string message, string Title = "", ArrayList list = null);
+        event showMes mesgg;
+
         public txt_url()
         {
             InitializeComponent();
             Control.CheckForIllegalCrossThreadCalls = false;
         }
 
-        Thread th;
-        long lines = 0;
-        ConcurrentQueue<JokeData> currendQueue = new ConcurrentQueue<JokeData>();
-
         private void txt_url_Load(object sender, EventArgs e)
         {
             this.linkLabel1.Text = "https://www.biedoul.com/index/1";
             this.linkLabel1.Links.Add(0, 0, "https://www.biedoul.com/index/1");
             this.toolTip1.SetToolTip(this.linkLabel1, "点击跳转到对应笑话网站！");
+
+            lb_zan.Text = "";
+            lb_buzan.Text = "";
+            lb_date.Text = "";
+
+            treeView1.Nodes.Clear();//清空treeView
+            mesgg += new showMes(showMessage);
+            Thread t = new Thread(new ThreadStart(GetDateAll));
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        public void showMessage(string Message, string Title, ArrayList list1)
+        {
+            if (!this.InvokeRequired)
+            {
+                ShowMessage mes = new ShowMessage(showMessage);
+                this.Invoke(mes, new object[] { Message, Title, list1 });
+            }
+            if (list1 != null)
+            {
+                list = list1;
+            }
+            if (!string.IsNullOrEmpty(Title))
+            {
+                treeView1.Invoke(new ShowMessage(addtreeView), new object[] { Message, Title, list });
+            }
+        }
+
+        public void addtreeView(string message, string Title, ArrayList list)
+        {
+            treeView1.Nodes.RemoveByKey(Title);
+            treeView1.Nodes.Add(Title, Title);
         }
 
         private void txt_url_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        /// <summary>
+        /// 获取数据集合
+        /// </summary>
+        public void GetDateAll()
+        {
+            var getAll = Db.Queryable<JokeData>().ToList();
+            foreach (var item in getAll)
+            {
+                list.Add(item);//放入list里
+                mesgg("", item.Title, list);
+            }
         }
 
         /// <summary>
@@ -63,75 +115,91 @@ namespace GrabJoke
         {
             try
             {
-                Output(DateTime.Now + " → " + " 程序开始执行！",Color.Blue);
+                Output(DateTime.Now + " → " + " 程序开始执行！", Color.Blue);
                 HtmlWeb web = new HtmlWeb();
                 web.OverrideEncoding = Encoding.GetEncoding("utf-8");
                 HtmlAgilityPack.HtmlDocument htmlDoc = web.Load(linkLabel1.Text);
                 HtmlNode strTemp = htmlDoc.DocumentNode.SelectSingleNode(@"/html/body/div[4]/div[1]/div[2]/a[6]");
                 if (strTemp == null)
                 {
-                    Output(DateTime.Now + " → " + " 获取总页数失败！" ,Color.Red);
+                    Output(DateTime.Now + " → " + " 获取总页数失败！", Color.Red);
                     return;
                 }
 
                 string str = CommonHelper.GetTitleContent(strTemp.OuterHtml, "a", "href");
                 if (string.IsNullOrEmpty(str))
                 {
-                    Output(DateTime.Now + " → " + " 获取总页数失败！",Color.Red);
+                    Output(DateTime.Now + " → " + " 获取总页数失败！", Color.Red);
                     return;
                 }
 
                 string result = Regex.Replace(str, @"[^0-9]+", "");
                 long pageCount = long.Parse(result);
-                Output(DateTime.Now + " → " + " 程序获取到总页数为：" + pageCount + "" ,Color.Blue);
-                for (int i = 1; i <= pageCount; i++)
+                Output(DateTime.Now + " → " + " 程序获取到总页数为：" + pageCount + "", Color.Blue);
+                for (int i = 11705; i <= pageCount; i++)
                 {
-                    rt_txt.AppendText(DateTime.Now + " → " + " 程序开始请求第" + i + "页数据！" + "\r\n");
-                    string url = string.Format("https://www.biedoul.com/index/{0}", i);
-                    web.OverrideEncoding = Encoding.GetEncoding("utf-8");
-                    HtmlAgilityPack.HtmlDocument html = web.Load(url);
-                    HtmlNodeCollection rtitle = html.DocumentNode.SelectNodes(@"//*[@class='xhlist']");//返回标题数据集合
-                    if (rtitle == null)
+                    try
                     {
-                        Output(DateTime.Now + " → " + " 未请求到数据，在第" + i.ToString() + "页,程序将睡眠5秒钟跳过本页请求数据！" ,Color.Red);
+                        rt_txt.AppendText(DateTime.Now + " → " + " 程序开始请求第" + i + "页数据！" + "\r\n");
+                        string url = string.Format("https://www.biedoul.com/index/{0}", i);
+                        web.OverrideEncoding = Encoding.GetEncoding("utf-8");
+                        HtmlAgilityPack.HtmlDocument html = web.Load(url);
+                        HtmlNodeCollection rtitle = html.DocumentNode.SelectNodes(@"//*[@class='xhlist']");//返回标题数据集合
+                        if (rtitle == null)
+                        {
+                            Output(DateTime.Now + " → " + " 未请求到数据，在第" + i.ToString() + "页,程序将睡眠5秒钟跳过本页请求数据！", Color.Red);
+                            Thread.Sleep(5000);
+                            continue;
+                        }
+                        else
+                        {
+                            foreach (HtmlNode node in rtitle)
+                            {
+                                try
+                                {
+                                    string title = node.SelectSingleNode(@"span/dd/a/strong").InnerText;//请求获取标题
+                                    string content = node.SelectSingleNode(@"dd").InnerHtml;//请求获取内容
+                                    string zanCount = node.SelectSingleNode(@"div/div[2]/a[1]/p").InnerText;//请求获取点赞数
+                                    string pointsCount = node.SelectSingleNode(@"div/div[2]/a[2]/p").InnerText;//请求获取减分数
+
+                                    var model = new JokeData()
+                                    {
+                                        Contents = content,
+                                        CreateBy = 1,
+                                        CreateDate = DateTime.Now,
+                                        PointsCount = long.Parse(pointsCount == "" ? "0" : pointsCount),
+                                        Title = title,
+                                        UpdateBy = 1,
+                                        UpdateDate = DateTime.Now,
+                                        ZanCount = long.Parse(zanCount == "" ? "0" : zanCount)
+                                    };
+                                    currendQueue.Enqueue(model);
+                                    Output(DateTime.Now + " → " + " 成功获取标题为：【" + title + "】已保存队列", Color.BlueViolet);
+                                }
+                                catch (Exception ex)
+                                {
+                                    rt_txt.AppendText(DateTime.Now + " → " + ex.Message + "\r\n");
+                                    continue;
+                                }
+                            }
+                        }
+
+                        RunConcurrentQueue(100);
+
+                        Output(DateTime.Now + " → " + " 第" + i + "页数据请求保存完毕！", Color.Black);
+                        Output(DateTime.Now + " → " + " 程序睡眠5秒钟，稍后开始请求下一页数据！", Color.Black);
                         Thread.Sleep(5000);
+                        GC.Collect();
+                    }
+                    catch (Exception ex)
+                    {
+                        rt_txt.AppendText(DateTime.Now + " → " + ex.Message + "\r\n");
                         continue;
                     }
-                    else
-                    {
-                        foreach (HtmlNode node in rtitle)
-                        {
-                            string title = node.SelectSingleNode(@"span/dd/a/strong").InnerText;//请求获取标题
-                            string content = node.SelectSingleNode(@"dd").InnerHtml;//请求获取内容
-                            string zanCount = node.SelectSingleNode(@"div/div[2]/a[1]/p").InnerText;//请求获取点赞数
-                            string pointsCount = node.SelectSingleNode(@"div/div[2]/a[2]/p").InnerText;//请求获取减分数
-
-                            var model = new JokeData()
-                            {
-                                Contents = content,
-                                CreateBy = 1,
-                                CreateDate = DateTime.Now,
-                                PointsCount = long.Parse(pointsCount == "" ? "0" : pointsCount),
-                                Title = title,
-                                UpdateBy = 1,
-                                UpdateDate = DateTime.Now,
-                                ZanCount = long.Parse(zanCount == "" ? "0" : zanCount)
-                            };
-                            currendQueue.Enqueue(model);
-                            Output(DateTime.Now + " → " + " 成功获取标题为：【" + title + "】已保存队列" ,Color.BlueViolet);
-                        }
-                    }
-
-                    RunConcurrentQueue(100);
-
-                    Output(DateTime.Now + " → " + " 第" + i + "页数据请求保存完毕！" ,Color.Black);
-                    Output(DateTime.Now + " → " + " 程序睡眠5秒钟，稍后开始请求下一页数据！" ,Color.Black);
-                    Thread.Sleep(5000);
-                    GC.Collect();
                 }
                 RunConcurrentQueue(1);
-                Output(DateTime.Now + " → " + " 所有页数据请求完毕，开始执行队列将数据保存至数据库...请稍后！" ,Color.Red);
-                Output(DateTime.Now + " → " + " 数据已保存完毕！",Color.Red);
+                Output(DateTime.Now + " → " + " 所有页数据请求完毕，开始执行队列将数据保存至数据库...请稍后！", Color.Red);
+                Output(DateTime.Now + " → " + " 数据已保存完毕！", Color.Red);
                 GC.Collect();
             }
             catch (Exception ex)
@@ -181,7 +249,7 @@ namespace GrabJoke
         /// <param name="e"></param>
         private void btn_getdata_Click(object sender, EventArgs e)
         {
-            Output(DateTime.Now + " → " + " 系统采用时间间隔执行，一天（24小时）执行一次，重复数据则更新，否则新增保存数据...执行期间请勿关闭程序！！！",Color.Blue);
+            Output(DateTime.Now + " → " + " 系统采用时间间隔执行，一天（24小时）执行一次，重复数据则更新，否则新增保存数据...执行期间请勿关闭程序！！！", Color.Blue);
             timer1.Interval = 1000 * 1 * 3600 * 24;
             timer1.Tick += new EventHandler(timer1_Tick);
             timer1.Enabled = true;
@@ -203,7 +271,7 @@ namespace GrabJoke
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            Output(DateTime.Now + " → " + " 程序暂停中...请不要关闭程序！",Color.Yellow);
+            Output(DateTime.Now + " → " + " 程序暂停中...请不要关闭程序！", Color.Yellow);
             if (th != null)
             {
                 th.Suspend();
@@ -217,7 +285,7 @@ namespace GrabJoke
         /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
-            Output(DateTime.Now + " → " + " 程序暂停恢复,继续执行...！",Color.Green);
+            Output(DateTime.Now + " → " + " 程序暂停恢复,继续执行...！", Color.Green);
             if (th != null)
             {
                 th.Resume();
@@ -262,6 +330,21 @@ namespace GrabJoke
                 this.rt_txt.SelectionStart = this.rt_txt.Text.Length;//设置插入符位置为文本框末
                 this.rt_txt.ScrollToCaret();//滚动条滚到到最新插入行
             }));
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            //显示对应得信息
+            foreach (JokeData rss in list)
+            {
+                if (rss.Title.Equals(e.Node.Text))
+                {
+                    lb_date.Text = rss.CreateDate.ToString("yyyy-MM-dd HH:mm:ss");
+                    lb_zan.Text = rss.ZanCount.ToString();
+                    lb_buzan.Text = rss.PointsCount.ToString();
+                    webBrowser2.DocumentText = rss.Contents;//因为Contents是html，所以应该使用这个来正常显示
+                }
+            }
         }
     }
 }
